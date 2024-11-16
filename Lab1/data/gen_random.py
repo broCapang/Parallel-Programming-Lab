3b6ce6038e601be0310f5fe1863a03cc742936c6
broCapang <U2000429@siswa.um.edu.my>
2024-11-16 15:44:33 +0800

    better data generator

--- Lab1/data/gen_random.py
+++ Lab1/data/gen_random.py
@@ -1,7 +1,14 @@
 import random
 
-# Generate a list of 50 random float values between 1.0 and 100.1
-random_floats = [round(random.uniform(1.0, 100.1), 1) for _ in range(100)]
+random_floats = [round(random.uniform(1.0, 100.1), 1) for _ in range(100000000)]
 
-for i in random_floats:
-    print(i)
+# Specify the file name
+file_name = "data100000000.txt"
+
+# Write the random floats to a text file
+with open(file_name, "w") as file:
+    for value in random_floats:
+        file.write(f"{value}\n")
+
+print(f"Random floats written to {file_name}")
+
